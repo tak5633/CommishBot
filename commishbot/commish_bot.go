57e5ef7002908bfed80fbdfee532482ba1661121
tak5633 <35234375+tak5633@users.noreply.github.com>
2024-03-03 22:09:17 -0800

    main - Feat(Week6): Initial Cut

--- commishbot/commish_bot.go
+++ commishbot/commish_bot.go
@@ -711,6 +711,57 @@ func Week5Summary(pLeagueInfo LeagueInfo) {
 //--------------------------------------------------------------------------------------------------
 //
 //--------------------------------------------------------------------------------------------------
+func Week6Summary(pLeagueInfo LeagueInfo) {
+
+   week := 6
+   criteria := "Photo Finish - Team With Closest Margin Of Victory"
+
+   var prizeEntries PrizeEntries
+
+   matchups := GetMatchups(pLeagueInfo.mLeague.League_id, week)
+
+   for _, roster := range pLeagueInfo.mRosters {
+
+      matchupRoster, err := GetMatchupRoster(matchups, roster.Roster_id)
+
+      if err != nil {
+         log.Printf("Week %d Summary: %s", week, err.Error())
+         return
+      }
+
+      matchupOpponentRoster, err :=  GetMatchupOpponentRoster(matchups, roster.Roster_id)
+
+      if err != nil {
+         log.Printf("Week %d Summary: %s", week, err.Error())
+         return
+      }
+
+      var prizeEntry PrizeEntry
+      prizeEntry.Owner = pLeagueInfo.mDisplayNames[roster.Owner_id]
+      prizeEntry.Score = math.Inf(1)
+
+      totalStarterPoints := GetTotalStarterPoints(matchupRoster)
+      totalOpponentStarterPoints := GetTotalStarterPoints(matchupOpponentRoster)
+
+      if totalStarterPoints > totalOpponentStarterPoints {
+         prizeEntry.Score = totalStarterPoints - totalOpponentStarterPoints
+      }
+
+      prizeEntries = append(prizeEntries, prizeEntry)
+   }
+
+   sort.Sort(prizeEntries)
+
+   log.Printf("Week %d Criteria: %s", week, criteria)
+
+   for _, prizeEntry := range prizeEntries {
+      log.Printf("   Owner: %s, Margin of Victory: %f", prizeEntry.Owner, prizeEntry.Score)
+   }
+}
+
+//--------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------
 func Week12Summary(pLeagueInfo LeagueInfo, pYear int) {
 
    week := 12
@@ -860,6 +911,7 @@ func main() {
       Week3Summary(leagueInfo)
       Week4Summary(leagueInfo)
       Week5Summary(leagueInfo)
+      Week6Summary(leagueInfo)
       Week12Summary(leagueInfo, config.Year)
       Week13Summary(leagueInfo)
       Week14Summary(leagueInfo, config.Year)
