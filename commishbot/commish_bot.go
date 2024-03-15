7a20460922e1f507e16c088cbe2ce19d0f587330
tak5633 <35234375+tak5633@users.noreply.github.com>
2024-03-14 21:11:55 -0700

    main - Refactor(ProjectedPlayerStats): Separate Files

--- commishbot/commish_bot.go
+++ commishbot/commish_bot.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"sort"
-	"strconv"
 )
 
 //--------------------------------------------------------------------------------------------------
@@ -32,92 +31,6 @@ func GetHttpResponse(pRequest string) string {
    check(err)
 
    return string(body)
-}
-
-//--------------------------------------------------------------------------------------------------
-//
-//--------------------------------------------------------------------------------------------------
-func GetProjectedPlayerStatsData(pPlayerId string, pYear int) string {
-   return GetHttpResponse("https://api.sleeper.com/projections/nfl/player/" + pPlayerId + "?season_type=regular&season=" + strconv.Itoa(pYear) + "&grouping=week")
-}
-
-//--------------------------------------------------------------------------------------------------
-//
-//--------------------------------------------------------------------------------------------------
-func GetProjectedPlayerStats(pPlayerId string, pYear int) map[string]json.RawMessage {
-
-   projectedPlayerStatsData := GetProjectedPlayerStatsData(pPlayerId, pYear)
-
-   var projectedPlayerStats map[string]json.RawMessage
-   err := json.Unmarshal([]byte(projectedPlayerStatsData), &projectedPlayerStats)
-   check(err)
-
-   return projectedPlayerStats
-}
-
-//--------------------------------------------------------------------------------------------------
-//
-//--------------------------------------------------------------------------------------------------
-func GetProjectedPlayerWeekStats(pPlayerId string, pYear int, pWeek int) (map[string]float64, error) {
-
-   yearStr := strconv.Itoa(pYear)
-   weekStr := strconv.Itoa(pWeek)
-
-   projectedPlayerStats := GetProjectedPlayerStats(pPlayerId, pYear)
-   projectedWeekData, hasKey := projectedPlayerStats[weekStr]
-
-   if !hasKey {
-      return nil, errors.New("Failed to retrieve " + yearStr + " week " + weekStr + " projections for player Id " + pPlayerId)
-   }
-
-   var projectedWeek map[string]json.RawMessage
-   err := json.Unmarshal([]byte(projectedWeekData), &projectedWeek)
-
-   if err != nil {
-      return nil, errors.New("Failed to unmarshal " + yearStr + " week " + weekStr + " projections for player Id " + pPlayerId)
-   }
-
-   projectedWeekStatsData, hasKey := projectedWeek["stats"]
-
-   if !hasKey {
-      return nil, errors.New("Failed to retrieve " + yearStr + " week " + weekStr + " stat projections for player Id " + pPlayerId)
-   }
-
-   var projectedWeekStats map[string]float64
-   err = json.Unmarshal([]byte(projectedWeekStatsData), &projectedWeekStats)
-
-   if err != nil {
-      return nil, errors.New("Failed to unmarshal " + yearStr + " week " + weekStr + " stat projections for player Id " + pPlayerId)
-   }
-
-   return projectedWeekStats, nil
-}
-
-//--------------------------------------------------------------------------------------------------
-//
-//--------------------------------------------------------------------------------------------------
-func GetProjectedPlayerWeekScore(pPlayerId string, pYear int, pWeek int, pScoringSettings map[string]json.RawMessage) (float64, error) {
-
-   projectedWeekStats, err := GetProjectedPlayerWeekStats(pPlayerId, pYear, pWeek)
-
-   if err != nil {
-      return 0.0, err
-   }
-
-   starterProjection := 0.0
-
-   for statKey, statValue := range projectedWeekStats {
-
-      scoringValue, err := GetScoringValue(pScoringSettings, statKey)
-
-      if err != nil {
-         return 0.0, err
-      }
-
-      starterProjection += scoringValue * statValue
-   }
-
-   return starterProjection, nil
 }
 
 //--------------------------------------------------------------------------------------------------
